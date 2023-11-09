e175749d38101e6687fb5737e76dc19c55910ee0
Enaholo Akhere <enaholoa@gmail.com>
2023-11-09 23:24:10 +0100

    :sparkles: FEAT(exit-intent): exit intent feature started

--- src/component/layout/layout.js
+++ src/component/layout/layout.js
@@ -2,10 +2,22 @@ import Box from '@mui/material/Box';
 import NavAppBar from './components/appbar';
 import Footer from './components/footer';
 import StickyBar from './components/stickybar';
+import { useState } from 'react';
+import Modal from '../../utils/Modal';
+import ExitIntent from '../forms/exit-intent';
 
 function Layout({ children, userData }) {
+  const [show, setShow] = useState(false);
+
   return (
-    <Box>
+    <Box
+      onMouseLeave={() => {
+        setShow(true);
+      }}
+    >
+      <Modal show={show} setShow={setShow}>
+        <ExitIntent show={show} setShow={setShow} />
+      </Modal>
       <Box>
         <StickyBar userData={userData} />
       </Box>
