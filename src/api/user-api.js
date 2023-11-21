fd5522f608fba8db2dce3191dfd6385cd8b1de35
Enaholo Akhere <enaholoa@gmail.com>
2023-11-21 21:12:25 +0100

    :bug: Feat(api): prod url bug

--- src/api/user-api.js
+++ src/api/user-api.js
@@ -7,7 +7,7 @@ export const api = process.env.REACT_APP_API_DEV;
 export const apiProd = process.env.REACT_APP_API_PROD;
 
 const baseUrl =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV !== 'development'
     ? process.env.REACT_APP_API_PROD
     : process.env.REACT_APP_API_DEV;
 
